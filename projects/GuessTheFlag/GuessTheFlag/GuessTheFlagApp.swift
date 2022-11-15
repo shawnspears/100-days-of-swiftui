79dbb855065cd3707e9edbf759237b5c1efa96d4
shawnspears <shspears@pdx.edu>
2022-11-14 19:09:53 -0800

    Initialize project 2

---
+++ projects/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Shawn Spears on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
