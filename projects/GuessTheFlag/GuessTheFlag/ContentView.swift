2e362cd4b9bbc46ea6445939d0955a4d767164ad
shawnspears <shspears@pdx.edu>
2022-11-16 23:56:00 -0800

    Add flag selected to alert message, keep track of questions asked and restart at 8

--- projects/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ projects/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
+    @State private var gameOver = false
     @State private var scoreTitle = ""
     @State private var score = 0
+    @State private var selected = 0
+    @State private var numQuestions = 0
      
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -68,7 +71,13 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is \(score)")
+            Text("That's the flag of \(countries[selected])! " +
+                 "Your score is \(score)")
+        }
+        .alert("Game Over", isPresented: $gameOver) {
+            Button("Restart", action: restartGame)
+        } message: {
+            Text("Your final score is \(score)")
         }
     }
     
@@ -78,13 +87,29 @@ struct ContentView: View {
             score += 1
         } else {
             scoreTitle = "Wrong"
+            score -= 1
         }
         showingScore = true
+        selected = number
+        numQuestions += 1
+        checkGameOver()
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func checkGameOver() {
+        if numQuestions == 8 {
+            gameOver = true
+        }
+    }
+    
+    func restartGame() {
+        gameOver = false
+        score = 0
+        numQuestions = 0
     }
 }
 
