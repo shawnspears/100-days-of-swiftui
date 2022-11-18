ba873208423367aab293bec654e21cf98e928ff0
shawnspears <shspears@pdx.edu>
2022-11-17 21:47:41 -0800

    Initialize project 3

---
+++ projects/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Shawn Spears on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
