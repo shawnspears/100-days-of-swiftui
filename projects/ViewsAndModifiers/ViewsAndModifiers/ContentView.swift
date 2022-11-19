0b84c44f8854bddef72dd1e74ab2877889a367a2
shawnspears <shspears@pdx.edu>
2022-11-18 23:52:57 -0800

    Add custom view modifier and extension for a large blue title

--- projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ projects/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,25 @@
 
 import SwiftUI
 
+struct BlueTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+            .padding()
+    }
+}
+
+extension View {
+    func blueTitleStyle() -> some View {
+        modifier(BlueTitle())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .blueTitleStyle()
     }
 }
 
