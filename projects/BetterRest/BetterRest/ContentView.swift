dae9651c718b320866b520f153a24989ba65a2cf
shawnspears <shspears@pdx.edu>
2022-11-22 19:21:43 -0800

    Replace vstacks with sections to improve layout and replace cups stepper with picker

--- projects/BetterRest/BetterRest/ContentView.swift
+++ projects/BetterRest/BetterRest/ContentView.swift
@@ -27,26 +27,27 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    
+                Section {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
+                } header: {
+                    Text("When do you want to wake up?")
                 }
                 
-                VStack(alignment: .leading, spacing: 0){
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    
+                Section {
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
+                Section {
+                    Picker(coffeeAmount == 1 ? "1 cup" :  "\(coffeeAmount) cups", selection: $coffeeAmount) {
+                        ForEach(0..<21) {
+                            Text($0 == 1 ? "1 cup" :"\($0) cups")
+                        }
+                    }
+                } header: {
                     Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Stepper(coffeeAmount == 1 ? "1 cup" :  "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
                 }
             }
             .navigationTitle("BetterRest")
