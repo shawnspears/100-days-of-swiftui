f748fc1e9343cd7c05bf496bfb542f34ffc4a37e
shawnspears <shspears@pdx.edu>
2022-11-23 23:23:52 -0800

    Initialize project 5

---
+++ projects/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Shawn Spears on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
