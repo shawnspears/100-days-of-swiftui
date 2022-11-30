69ede71bc9a45d7be87d03a51c40d1c9036dd4ff
shawnspears <shspears@pdx.edu>
2022-11-29 20:48:40 -0800

    Add challenge project for day 35

---
+++ projects/ZooTables/ZooTables/ZooTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZooTablesApp.swift
+//  ZooTables
+//
+//  Created by Shawn Spears on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ZooTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
