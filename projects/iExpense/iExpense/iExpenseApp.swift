2ed3d6a81abf452d56bbad37e11bc4b4706f7255
shawnspears <shspears@pdx.edu>
2022-11-30 21:48:45 -0800

    Add project 7

---
+++ projects/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Shawn Spears on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
